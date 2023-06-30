5703f1b9691ebe4636eeb1a0c5bd1a25f2a82a6d
heexohee <rsoy2918@naver.com>
2023-06-30 17:51:09 +0900

    map함수

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 import { styled } from 'styled-components';
 
+
+const StContainer = styled.div`
+display: flex;
+
+
+`
+
+
 //태그로서 사용이 될 수 있는 컴포넌트가 된 것.
 //그래서 스타일된 컴포넌트.
 //스타일드 컴포넌트의 장점이 조건적으로 스타일링 할 수 있다!
@@ -12,21 +20,41 @@ border: 1px solid ${(props) => {
   return props.borderColor
   }};
 margin: 20px;
-background-color: 
-${(props)=> props.backgroundColor
-};
+
 `
 
 
+// 박스의 색을 배열 형태로 선언
+const boxList = ['red', 'blue', 'green']
 
 
+//색을 넣으면 이름을 반환 -> 여러줄의 코드를 한줄로 줄일 수 있다.
+const getBoxName =(color)=>{
+  switch (color) {
+    case 'red' : 
+    return "빨간상자님";
+
+    case 'blue' : 
+    return "파랑상자님";
+
+    case 'green' : 
+    return "초록상자님";
+
+    default:
+      return '검정상자님';
+  }
+}
 function App() {
   return (
-    <>
-    <StBox borderColor='red' backgroundColor='yellow'> 빨간상자님</StBox>
-    <StBox borderColor='blue' > 파랑상자님</StBox>
-    <StBox borderColor='green' > 초록상자님</StBox>
-    </>
+    <StContainer>
+    {/* <StBox borderColor='red' > 빨간상자님</StBox> */}
+    {
+      boxList.map((box)=>{
+        return <StBox borderColor={box}>{getBoxName(box)}</StBox>
+        
+      })
+    }
+    </StContainer>
   );
 }
 // props : 부모 컴-> 자식 컴에서 내려주는 데이터
