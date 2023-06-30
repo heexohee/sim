5a420e4922ae430af386a716f39b6f831336908e
heexohee <rsoy2918@naver.com>
2023-06-30 16:59:43 +0900

    기초 컴포넌트 스타일 1강

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { styled } from 'styled-components';
+
+//태그로서 사용이 될 수 있는 컴포넌트가 된 것.
+//그래서 스타일된 컴포넌트.
+//스타일드 컴포넌트의 장점이 조건적으로 스타일링 할 수 있다!
+// input은 props가 들어오고, props에는 컴포넌틑에 정해줬던 props값이 무조건 들어간다.
+const StBox = styled.div`
+width: 100px;
+height: 100px;
+border: 1px solid ${(props) => {
+  return props.borderColor
+  }};
+margin: 20px;
+background-color: 
+${(props)=> props.backgroundColor
+};
+`
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <StBox borderColor='red' backgroundColor='yellow'> 빨간상자님</StBox>
+    <StBox borderColor='blue' > 파랑상자님</StBox>
+    <StBox borderColor='green' > 초록상자님</StBox>
+    </>
   );
 }
-
+// props : 부모 컴-> 자식 컴에서 내려주는 데이터
 export default App;
